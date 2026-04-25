f80dea22bc83ffd5c7a560f9de9af4b7125beb52
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-04-25 17:57:30 +0530

    Integrate dotenv for environment variable management
    Added dotenv configuration to load environment variables.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-// server.js
 import express from "express";
+import dotenv from "dotenv";
 import mqttRoutes from "./routes/mqtt.js";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
