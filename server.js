131b3503592e41e4609711e56f76d4cd0ee2c08e
syaswanthkumar66-hash <syaswanthkumar66@gmail.com>
2026-04-26 17:15:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ import dotenv from "dotenv";
 import mqttRoutes from "./routes/mqtt.js";
 import factoryRoutes from "./routes/factory.js";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/user.js"; 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors()); // quick fix
 app.use("/api/v1/auth", authRoutes);
-
+app.use("/api/v1/user", userRoutes); 
 app.use("/api/v1/mqtt", mqttRoutes);
 app.use("/api/v1/factory", factoryRoutes);
 
